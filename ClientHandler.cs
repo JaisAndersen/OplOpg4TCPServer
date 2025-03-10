f37179a7c99702548bf74d2bdd055ab9fbe94031
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-10 11:33:23 +0100

    Added ClientHandler Class

---
+++ ClientHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oplopg4tcpserver
+{
+    public class ClientHandler
+    {
+        public static void HandleClient(TcpClient client)
+        {
+            Console.WriteLine(client.Client.RemoteEndPoint);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                string? message = reader.ReadLine();
+                Console.WriteLine(message);
+                if (message == "Random")
+                {
+                    writer.WriteLine("Input Numbers");
+                    writer.Flush();
+                    string? dicePick = reader.ReadLine();
+                    string[] dicePickSplit = dicePick.Split(' ');
+                    if (dicePick != null)
+                    {
+                        int dicePickParsedFirst = Int32.Parse(dicePickSplit[0]);
+                        int dicePickParsedSecond = Int32.Parse(dicePickSplit[1]);
+                        Random random = new Random();
+                        writer.WriteLine(random.Next(dicePickParsedFirst, dicePickParsedSecond + 1));
+                        writer.Flush();
+                    }
+                }
+            }
+            client.Close();
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using oplopg4tcpserver;
+using System.Net.Sockets;
+using System.Net;
+
+Console.WriteLine("TCP Server:");
+int port = 7;
+TcpListener listener = new TcpListener(IPAddress.Any, port);
+listener.Start();
+
+while (true)
+{
+    TcpClient client = listener.AcceptTcpClient();
+    Task.Run(() => ClientHandler.HandleClient(client));
+}
